b1c1cad94601ec3d431c799a5af1b901cedaeec5
Faisal L <salluthdev@gmail.com>
2021-10-11 17:18:10 +0700

    Pembersihan Kode JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,5 @@
 let menutoggle = document.querySelector('.toggle');
 let navigation = document.querySelector('.navigation');
-let linkUrl = document.getElementsByClassName('link');
-let x=false;
-for (let i = 0; i < linkUrl.length; i++) {
-	linkUrl[i].onclick = function(){
-		if (x){
-			menutoggle.classList.toggle('active');
-			navigation.classList.toggle('active');
-			x=!(x);
-		}
-	}
-}
-menutoggle.onclick = function(){
-	menutoggle.classList.toggle('active');
-	navigation.classList.toggle('active');
-	x=!(x);
-}
 
 $(document).ready(function(){
 	// Jquery for Toggle Sub-Menu
