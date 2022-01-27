b8cee38bb5796a7b342a30c34bdee3761335a0cc
Faisal L <salluthdev@gmail.com>
2022-01-27 18:01:10 +0700

    Pembersihan Kode Script JS

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,6 @@ let menutoggle = document.querySelector('.toggle');
 let navigation = document.querySelector('.navigation');
 
 $(document).ready(function(){
-	// Jquery for Toggle Sub-Menu
-	$('.sub-btn').click(function(){
-		$(this).next('.sub-menu').slideToggle();
-		$(this).find('.dropdown').toggleClass('rotate')
-	});
-
-	// Jquery for Expand & Collapse The Sidebar
 	$('.menu-btn').click(function(){
 		$('.side-bar').addClass('active');
 		$('.menu-btn').css("visibility", "hidden")
